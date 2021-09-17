9e9553a3294e9fca284c64a95843f8aff4ef34b6
ruggerygusmao <ruggery402@gmail.com>
2021-09-16 22:35:18 -0300

    atividade de fixação

---
+++ atividade01.js
@@ -0,0 +1,20 @@
+/*1) Criar variáveis com tipos de dados:
+a) Number b) String c) Boolean d) Array
+e) Object f) Undefined g) Null */
+
+//a) Number
+var idade = 24;
+//b) String
+var nome = "Ruggery";
+//c) Boolean
+var status = true;
+//d) Array
+var cores = ["azul", "preto", "cinza"];
+//e) Object
+var moto = {cor: "preta", modelo:"ESD", ano: 2015, ehSeminovo: true};
+//f) Undefined
+var indefinido;
+//g) Null
+var vazio = null;
+
+console.log(cores)
