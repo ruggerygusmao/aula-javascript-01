5418ddfa026dcbc8ce518d79946dae8e0cec3929
ruggerygusmao <ruggery402@gmail.com>
2021-09-16 22:36:50 -0300

    atividade de fixação

---
+++ atividade02.js
@@ -0,0 +1,20 @@
+/* 1)      Criar duas variáveis usando a palavra-chave var.
+2)      Criar duas variáveis usando a palavra-chave let.
+3)      Criar duas constantes usando a palavra-chave const. */
+
+var nome1 = "Gusmao";
+var nome2 = "Alves";
+
+let dog = "lulu";
+let cat = "nulk"
+
+const DIVISAO = 2;
+const PI =  3.14159;
+
+var x=2;
+
+if(x>=1){
+    let y =5 ;
+    console.log(y);
+}
+
